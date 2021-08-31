a0962d0b0ceb9c209368b4f4cddd1cafc4673531
Anupam Parashar <anuparashar@gmail.com>
2021-08-31 07:05:51 +0530

    comit3

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import { createGlobalStyle, ThemeProvider } from "styled-components";
-import theme from "./theme";
+import theme from "../components/theme";
 
 const GlobalStyle = createGlobalStyle`
 html,
